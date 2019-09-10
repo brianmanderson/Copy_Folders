f1de689df33cedb196c8a14b47184a02409abd95
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-09-10 14:32:10 -0500

    generalized

--- Copy_Folder_To_Another.py
+++ Copy_Folder_To_Another.py
@@ -33,7 +33,7 @@ def down_folder(input_path,output=r'\\mymdafiles\di_data1\Morfeus\Andrea\Copy_Lo
 
 if __name__ == '__main__':
     xxx = 1
-    # output = r'\\mymdafiles\di_data1\Morfeus\bmanderson\Copy_Of_Sanne_Patients'
-    # base_path = r'R:\Team Share\Sam_Mulder\P_F_X\MATLAB_Practice\OutputNEWEST\DIXON'
-    # folder_criteria = lambda i: i.find('RTSTRUCT') != -1
+    # output = r'\\path_to\output_images'
+    # base_path = r'R:\path_to\current_images'
+    # folder_criteria = lambda i: i.find('RTSTRUCT') != -1 # If the folder has the title 'RTSTRUCT' in it, copy
     # down_folder(base_path, output=output, base_path=base_path, folder_criteria=folder_criteria)
