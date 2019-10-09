88cdc893ed469f9938c641c9d8bb4b0c913a48a9
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-10-09 14:58:55 -0500

    added file criteria option and stand alone down_copy

--- Copy_Folder_To_Another.py
+++ Copy_Folder_To_Another.py
@@ -1,23 +1,40 @@
 import shutil, os
 
-class Copy_Folders(object):
-    def __init__(self, input_path, output_path, folder_criteria=lambda x: True):
-        self.down_folder(input_path,output=output_path,base_path=input_path,folder_criteria=folder_criteria)
 
-    def down_copy(self, input_path,output_path):
+def down_copy(input_path, output_path, file_criteria=lambda x:True):
+    dirs = []
+    files = []
+    for _, dirs, files in os.walk(input_path):
+        break
+    for file in files:
+        if file_criteria(file):
+            shutil.copy(os.path.join(input_path, file), os.path.join(output_path, file))
+    for dir_val in dirs:
+        new_out = os.path.join(output_path, dir_val)
+        if not os.path.exists(new_out):
+            os.makedirs(new_out)
+        down_copy(os.path.join(input_path, dir_val), new_out)
+
+class Copy_Folders(object):
+    def __init__(self, input_path, output_path, folder_criteria=lambda x: True, file_criteria=lambda x:True):
+        self.down_folder(input_path,output=output_path,base_path=input_path,folder_criteria=folder_criteria, file_criteria=file_criteria)
+
+    def down_copy(self, input_path,output_path, file_criteria=lambda x:True):
         dirs = []
         files = []
         for _, dirs, files in os.walk(input_path):
             break
         for file in files:
-            shutil.copy(os.path.join(input_path,file),os.path.join(output_path,file))
+            if file_criteria(file):
+                shutil.copy(os.path.join(input_path,file),os.path.join(output_path,file))
         for dir_val in dirs:
             new_out = os.path.join(output_path,dir_val)
             if not os.path.exists(new_out):
                 os.makedirs(new_out)
             self.down_copy(os.path.join(input_path,dir_val),new_out)
 
-    def down_folder(self, input_path,output=r'\\mymdafiles\di_data1\Morfeus\Andrea\Copy_Logs',base_path=r'G:\Cat',folder_criteria=lambda x:True):
+    def down_folder(self, input_path,output=r'\\mymdafiles\di_data1\Morfeus\Andrea\Copy_Logs',base_path=r'G:\Cat',folder_criteria=lambda x:True,
+                    file_criteria=lambda x:True):
         dirs = []
         for _, dirs, _ in os.walk(input_path):
             break
@@ -27,9 +44,10 @@ class Copy_Folders(object):
                 if not os.path.exists(new_output):
                     os.makedirs(new_output)
                 print(new_output)
-                self.down_copy(os.path.join(input_path,dir_val), new_output)
+                self.down_copy(os.path.join(input_path,dir_val), new_output, file_criteria=file_criteria)
             else:
-                self.down_folder(os.path.join(input_path,dir_val),output=output,base_path=base_path, folder_criteria=folder_criteria)
+                self.down_folder(os.path.join(input_path,dir_val),output=output,base_path=base_path,
+                                 folder_criteria=folder_criteria, file_criteria=file_criteria)
         return None
 
 
